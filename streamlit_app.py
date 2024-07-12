449f0b8329303cdd4624e09ca66a1ac0af186c9b
neuracap <134765615+neuracap@users.noreply.github.com>
2024-07-12 23:15:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Chart Patterns")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "See which all stocks are making patterns that you like"
 )
